928bef40516f709245c8411697c7bc8be1e3a117
Yogesh satyam <yogesh.satyam23@gmail.com>
2022-04-14 01:58:50 +0530

    about us page front end completed

--- phome/models.py
+++ phome/models.py
@@ -15,7 +15,7 @@ class Cuisines_category(models.Model):
     name=models.CharField(null=True,max_length=50)
     actual_price=models.CharField(default=None, null=True,max_length=50)
     price_range=models.CharField(default=None, null=True,max_length=50)
-    extra_offer=models.CharField(default=None,null=True,max_length=100)
+    extra_offer=models.CharField(default=None,null=True,blank=True,max_length=100)
     image = models.ImageField(default=None, upload_to=filepath, null=True, blank=True)
     discount=models.CharField(default=None, null=True,max_length=50)
     
